173a77e0d56cf6ee836e1308148d1d7531083d3e
Satyanarayana Devi <connect2satyam@gmail.com>
2022-02-02 14:13:05 +0530

    small

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,5 @@
 import express from 'express';
-
 import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
-
 const router = express.Router();
 
 router.get('/', getPosts);
