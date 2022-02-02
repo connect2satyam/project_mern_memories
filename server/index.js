42aeecd6b6afb9856e5ded9ff92ea6faaac62139
Satyanarayana Devi <connect2satyam@gmail.com>
2022-02-02 14:20:12 +0530

    small

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://connect2satyam:6Lakshmi@@cluster0.kl75o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://connect2satyam:6Lakshmi@@cluster0.kl75o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
